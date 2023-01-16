907d4aa36f0c7faac50a3a69ca6fbee263c1779f
SourabhB266 <122530957+SourabhB266@users.noreply.github.com>
2023-01-16 09:21:20 +0530

    Many to many hibernate project

---
+++ Person.java
@@ -0,0 +1,67 @@
+package com.ty_many_to_many_person.dto;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Person {
+	@Id
+	private int id;
+	private String name;
+	private String address;
+	private long phone;
+	
+	@ManyToMany
+	private List<Language> langaues;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public List<Language> getLangaues() {
+		return langaues;
+	}
+
+	public void setLangaues(List<Language> langaues) {
+		this.langaues = langaues;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + ", langaues="
+				+ langaues + "]";
+	}
+	
+	
+}
